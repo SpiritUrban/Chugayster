8f24e6ba0ce512efc1a417d2fcbdb438270269ed
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2019-08-06 02:35:48 +0300

    add bootstrap; add nav;

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './root/app.component';
 import { MainComponent } from './pages/main/main.component';
 import { P404Component } from './pages/p404/p404.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { NavComponent } from './components/general/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     P404Component,
-    AboutUsComponent
+    AboutUsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
