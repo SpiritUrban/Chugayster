fcd63dcb85423ecb02e5a81375e475db569101ba
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2019-08-11 02:11:57 +0300

    correct alerts;

--- front/src/app/@modules/@authentication/pages/register/register.component.ts
+++ front/src/app/@modules/@authentication/pages/register/register.component.ts
@@ -13,12 +13,14 @@ import { log } from '../../../../my_modules/stuff';
 export class RegisterComponent implements OnInit {
 
   userForm: FormGroup;
+
   roles: Array<string> = [
     'Guest',
     'Admin',
     'Owner',
     'Operator'
   ];
+
   user: User = {
     firstName: '',
     lastName: '',
@@ -34,14 +36,19 @@ export class RegisterComponent implements OnInit {
     private formBuilder: FormBuilder,
     // private api: ApiService
   ) {
-    const pwdValidators: ValidatorFn[] = [Validators.required, Validators.minLength(6), Validators.maxLength(20)];
 
+    const pwdValidators: ValidatorFn[] = [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(20),
+      // this.passwordConfirm()
+    ];
 
     this.userForm = this.formBuilder.group({
       'email': [this.user.email, [Validators.required, Validators.minLength(5), this.mailValidator()]],
-      'firstName': [this.user.firstName, [Validators.required, Validators.minLength(3)]],
-      'lastName': [this.user.lastName, [Validators.required]],
-      'password': [this.user.password, [Validators.required, this.passwordConfirm()]],
+      // 'password': [this.user.password, [Validators.required, this.passwordConfirm()]],
+      // 'firstName': [this.user.firstName, [Validators.required, Validators.minLength(3)]],
+      // 'lastName': [this.user.lastName, [Validators.required]],
       //'password1': [this.user.password, [Validators.required,Validators.minLength(3),this.passwordsAreEqual()]],
 
       'passwords': this.formBuilder.group({
@@ -50,8 +57,8 @@ export class RegisterComponent implements OnInit {
       }, { validator: this.passwordsAreEqual() }),
 
       //'password2': [this.user.password, [Validators.required,Validators.minLength(3),this.passwordsAreEqual()]],
-      'role': [this.user.role, [Validators.required]],
-      'notes': [this.user.notes, [Validators.maxLength(45)]]
+      // 'role': [this.user.role, [Validators.required]],
+      // 'notes': [this.user.notes, [Validators.maxLength(45)]]
     });
   }
 
@@ -59,35 +66,21 @@ export class RegisterComponent implements OnInit {
   private mailValidator(): ValidatorFn {
     const pattern: RegExp = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     return (control: AbstractControl): { [key: string]: any } => {
-      if (!(control.dirty || control.touched)) {
-        return null;
-      } else {
-        return pattern.test(control.value) ? null : { custom: `Invalid email` };
-      }
+      if (!(control.dirty || control.touched)) return null
+      else return pattern.test(control.value) ? null : { custom: `Invalid email` };
     };
   }
+
   //check password
   private passwordConfirm(): ValidatorFn {
-    return (group: FormGroup): { [key: string]: any } => {
-      if (!(group.dirty || group.touched)) {
-        return { custom: 'Something going wrong' }
-      } else {
-        return null;
-      }
-
-    };
+    return (group: FormGroup) => (!(group.dirty || group.touched)) ? { custom: 'Something going wrong' } : null
   }
 
   //check password equal
   private passwordsAreEqual(): ValidatorFn {
     return (group: FormGroup): { [key: string]: any } => {
-      console.log(group.get('password'))
-      if (!(group.dirty || group.touched) || group.get('pwd').value === group.get('confirm').value) {
-        return null;
-      }
-      return {
-        custom: 'Passwords are not equal'
-      };
+      if (!(group.dirty || group.touched) || group.get('pwd').value === group.get('confirm').value) return null;
+      return { custom: 'Passwords are not equal' }; //=> this.userForm.controls.passwords.errors.custom
     };
   }
 
@@ -99,9 +92,14 @@ export class RegisterComponent implements OnInit {
   logForm() {
     const userData = {
       email: this.userForm.controls.email.value,
-      password: this.userForm.controls.password.value
+      password1: this.userForm.controls.passwords.controls.pwd.value,
+      password2: this.userForm.controls.passwords.controls.confirm.value
+
     }
     log(userData)
+    log(this.userForm.controls.passwords.errors)
+    log(this.userForm.controls.passwords.controls.pwd.errors)
+    // userForm.controls.passwords.errors.custom
   }
 
 }
