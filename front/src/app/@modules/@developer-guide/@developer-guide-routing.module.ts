a1e11f85a11fbc8be9ac9e596e16d69d1e369dc8
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2019-08-12 17:42:53 +0300

    routing for 'UrlQueriesComponent';

--- front/src/app/@modules/@developer-guide/@developer-guide-routing.module.ts
+++ front/src/app/@modules/@developer-guide/@developer-guide-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { FormsComponent } from './pages/forms/forms.component';
+import { UrlQueriesComponent } from './pages/url-queries/url-queries.component'
 
 
 const routes: Routes = [
   { path: 'forms', component: FormsComponent },
-
+  { path: 'url-queries', component: UrlQueriesComponent },
 ];
 
 @NgModule({
