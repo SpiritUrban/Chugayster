a96fa138f6f1d40afed8c331713ef51357e3fddd
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2019-08-13 17:52:27 +0300

    fouting for 'any' page;

--- front/src/app/@modules/@developer-guide/@developer-guide-routing.module.ts
+++ front/src/app/@modules/@developer-guide/@developer-guide-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// pages
 import { FormsComponent } from './pages/forms/forms.component';
 import { UrlQueriesComponent } from './pages/url-queries/url-queries.component'
+import { AnyComponent } from './pages/any/any.component'
 
 
 const routes: Routes = [
   { path: 'forms', component: FormsComponent },
   { path: 'url-queries', component: UrlQueriesComponent },
+  { path: 'any', component: AnyComponent },
 ];
 
 @NgModule({
