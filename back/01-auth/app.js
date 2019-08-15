980fed888177c33a7e85011f0e49cde7b65f0236
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2019-08-15 19:21:47 +0300

    added clean 'express.js' for 'auth' microservice; clipped; refactored;

--- back/01-auth/app.js
+++ back/01-auth/app.js
@@ -19,14 +19,10 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+app.use((err, req, res, next) => {
+  console.log('\n\n app.js > ERROR: \n\n', err);
   res.status(err.status || 500);
-  res.render('error');
+  res.end(err.message)
 });
 
 module.exports = app;
--- back/01-auth/routes/index.js
+++ back/01-auth/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.end('The AUTH API. Port: 49001. Endpoints: /reg, /log')
 });
 
 module.exports = router;
