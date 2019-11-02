90a2e331816381c2522e548368114f655cd1a5dd
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2019-11-03 00:50:35 +0200

    fundamental connection other projects on to Chugayster

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,27 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// We have connected other active projects here
+const projectMode = 'JS-Clan';
 
-const routes: Routes = [
-  {
+const routes: Routes = []
+
+if (projectMode == 'JS-Clan') {
+  routes.push({
+    path: '',
+    loadChildren: './@modules/js-clan/js-clan.module#JsClanModule'
+  })  
+}else{
+  routes.push({
     path: '',
     loadChildren: './@modules/@common-dependencies/@common-dependencies.module#CommonDependenciesModule'
-  },
+  })
+}
+
   // {
   //   path: 'login',
   //   loadChildren: './@modules/@authentication/@authentication.module#AuthenticationModule',
   // },
-  {
-    path: 'auth',
-    loadChildren: () => import('./@modules/@authentication/@authentication.module').then(mod => mod.AuthenticationModule)
-  },
-  {
-    path: 'dev',
-    loadChildren: () => import('./@modules/@developer-guide/@developer-guide.module').then(mod => mod.DeveloperGuideModule)
-  },
-  { path: '**', redirectTo: '/p404' },
 
-];
+  routes.push(
+    {
+      path: 'auth',
+      loadChildren: () => import('./@modules/@authentication/@authentication.module').then(mod => mod.AuthenticationModule)
+    },
+    {
+      path: 'dev',
+      loadChildren: () => import('./@modules/@developer-guide/@developer-guide.module').then(mod => mod.DeveloperGuideModule)
+    },
+    { path: '**', redirectTo: '/p404' }
+  )
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
