e3200c62021c8852b2777e8f9741f5154cab33fb
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2019-12-16 18:45:31 +0200

    lesson page

--- front/src/app/@modules/@js-clan/pages/lesson/lesson.component.ts
+++ front/src/app/@modules/@js-clan/pages/lesson/lesson.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import  state  from '../../../../app-state';
 import { log } from '../../../../my_modules/stuff';
+import { ApiService } from '../../../@common-dependencies/services/api.service';
 
 @Component({
   selector: 'app-lesson',
@@ -10,14 +11,38 @@ import { log } from '../../../../my_modules/stuff';
 })
 export class LessonComponent implements OnInit {
 
+  st = {
+    lang: 'ua',
+    cards: <any>[],
+    currentCard: <any>{
+      title: '',
+      description: ''
+    }
+  }
+
   constructor(
     private route: ActivatedRoute,
+    private api: ApiService
   ) { }
 
-  ngOnInit() {
-    let name = this.route.snapshot.paramMap.get('name');
-    log(name)
-    log(state)
+  async ngOnInit() {
+    // 1
+    let lessonName = this.route.snapshot.paramMap.get('name');
+    log(lessonName)
+    // log(state)
+
+
+    // 2
+    try {
+      this.st.cards = await this.api.getJson('/js-clan/data/lessons.json');
+      console.log(this.st.cards);
+    } catch (error) {
+      console.log(error);
+    }
+
+    //3
+    this.st.currentCard = this.st.cards.filter( card => card.name == lessonName )[0]
+    log(this.st.currentCard)
 
   }
 
