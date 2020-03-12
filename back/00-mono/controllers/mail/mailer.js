2cd9a78f5031e8f6075eee96e7ad8489b9f5d0be
SpiritUrban <shadespiritenator@gmail.com>
2020-03-13 01:27:59 +0200

    fix mailer

--- back/00-mono/controllers/mail/mailer.js
+++ back/00-mono/controllers/mail/mailer.js
@@ -2,18 +2,24 @@
 // Хороший простий опис - https://codeburst.io/sending-an-email-using-nodemailer-gmail-7cfa0712a799
 // Додатково - http://qaru.site/questions/495891/sending-email-via-nodejs-using-nodemailer-is-not-working
 
-
+// Потрібно відкривати доступ тут, щоб працювало - https://myaccount.google.com/u/4/lesssecureapps?pageId=none
 
 const nodemailer = require('nodemailer');
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // use SSL
+    // host: 'smtp.gmail.com',
+    // port: 465,
+    // port: 25,
+    // secure: true, // use SSL
+    // secure: false, // use SSL
+    service: 'Gmail',
     auth: {
         user: process.env.GMAIL,
         pass: process.env.GMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
     },
     connectionTimeout: 1 * 60 * 1000
 })
