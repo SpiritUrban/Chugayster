bd39fbec95f1281fdc3fd3ace1eafc09761ce4dd
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2020-04-08 17:42:08 +0300

    refactor

--- front/src/app/@modules/@js-clan/pages/games/ar/ar.component.ts
+++ front/src/app/@modules/@js-clan/pages/games/ar/ar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ApiService } from '../../../../@common-dependencies/services/api.service';
 import { log } from 'src/app/my_modules/stuff';
 import appState from '../../../../../app-state';
+declare let AFRAME: any;
 
 declare var window: any;
 @Component({
@@ -13,102 +14,95 @@ declare var window: any;
 export class ArComponent implements OnInit {
 
   fireFlow: any; // setInterval // fire loop
+  sceneEl = document.querySelector('a-scene');
+  constraints = window.constraints = {
+    audio: false,
+    video: true
+  };
 
   constructor() { }
 
   ngOnInit(): void {
-
     // 40
-    setInterval(() => {
-      this.aimMove()
-    }, 100)
+    setInterval(() => this.aimMove, 100);
 
+    document.querySelector('#showVideo').addEventListener('click', e => this.init(e));
+    document.querySelector('a-scene').addEventListener('loaded', this.aFrameOnInit);
+  }
 
-    const constraints = window.constraints = {
-      audio: false,
-      video: true
-    };
+  aFrameOnInit(){
+    alert('LOADED')
+    this.x();
+    // setInterval(() => {
+    //   this.spawn()
+    // }, 15000)
+    this.spawn('enemy');
+    this.spawn('enemy');
+  }
 
-    function handleSuccess(stream) {
-      const video = document.querySelector('video');
-      const videoTracks = stream.getVideoTracks();
-      console.log('Got stream with constraints:', constraints);
-      console.log(`Using video device: ${videoTracks[0].label}`);
-      window.stream = stream; // make variable available to browser console
-      video.srcObject = stream;
+  async init(e) {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia(this.constraints);
+      this.handleSuccess(stream);
+      e.target.disabled = true;
+    } catch (e) {
+      this.handleError(e);
     }
+  }
 
-    function handleError(error) {
-      if (error.name === 'ConstraintNotSatisfiedError') {
-        const v: any = constraints.video;
-        errorMsg(`The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`, 1);
-      } else if (error.name === 'PermissionDeniedError') {
-        errorMsg('Permissions have not been granted to use your camera and ' +
-          'microphone, you need to allow the page access to your devices in ' +
-          'order for the demo to work.', 1);
-      }
-      errorMsg(`getUserMedia error: ${error.name}`, error);
+  handleSuccess(stream) {
+    const video = document.querySelector('video');
+    const videoTracks = stream.getVideoTracks();
+    console.log('Got stream with constraints:', this.constraints);
+    console.log(`Using video device: ${videoTracks[0].label}`);
+    window.stream = stream; // make variable available to browser console
+    video.srcObject = stream;
+  }
+
+  handleError(error) {
+    if (error.name === 'ConstraintNotSatisfiedError') {
+      const v: any = this.constraints.video;
+      this.errorMsg(`The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`, 1);
+    } else if (error.name === 'PermissionDeniedError') {
+      this.errorMsg('Permissions have not been granted to use your camera and ' +
+        'microphone, you need to allow the page access to your devices in ' +
+        'order for the demo to work.', 1);
     }
+    this.errorMsg(`getUserMedia error: ${error.name}`, error);
+  }
 
-    function errorMsg(msg, error) {
-      const errorElement = document.querySelector('#errorMsg');
-      errorElement.innerHTML += `<p>${msg}</p>`;
-      if (typeof error !== 'undefined') {
-        console.error(error);
-      }
+  errorMsg(msg, error) {
+    const errorElement = document.querySelector('#errorMsg');
+    errorElement.innerHTML += `<p>${msg}</p>`;
+    if (typeof error !== 'undefined') {
+      console.error(error);
     }
-
-    async function init(e) {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        handleSuccess(stream);
-        e.target.disabled = true;
-      } catch (e) {
-        handleError(e);
-      }
-    }
-
-    document.querySelector('#showVideo').addEventListener('click', e => init(e));
-
-
-    document.querySelector('a-scene').addEventListener('loaded', () => {
-      alert('LOADED')
-      this.x()
-
-      setInterval(() => {
-        this.spawn()
-      }, 15000)
-
-    })
-
   }
 
   x() {
-
     // Add boxe when spacebar is pressed.
     document.addEventListener('keyup', (e) => {
       if (e.keyCode !== 32) return;
-      this.spawn()
-
+      this.spawn('enemy');
     });
   }
 
   info(info) {
     alert(info)
   }
+  
 
-  spawn() {
-    var sceneEl = document.querySelector('a-scene');
-    const m: any = document.querySelector('#marker')
-    const position = m.object3D.getWorldPosition()
+  spawn(type) {
+    const marker: any = document.querySelector('#marker')
+    const position = marker.object3D.getWorldPosition()
     // enemy
     var newEl = document.createElement('a-entity');
-    newEl.setAttribute('class', 'enemy');
+    newEl.setAttribute('class', type);
     newEl.setAttribute('position', '-10 0.5 -20');
     newEl.setAttribute('scale', '40 40 40');
     newEl.setAttribute('gltf-model', 'url(assets/js-clan/3d/biotronican_crab_head_c1/scene.gltf)');
 
-    sceneEl.appendChild(newEl);
+    this.sceneEl.appendChild(newEl);
     // var position = this.getMarkerPosition()
     log(position)
     position.y = '0.5';
@@ -118,7 +112,7 @@ export class ArComponent implements OnInit {
   fireStart() {
     log('firestart')
     this.fireFlow = setInterval(() => {
-      this.spawn()
+      this.spawn('enemy');
     }, 500)
   }
 
@@ -127,7 +121,6 @@ export class ArComponent implements OnInit {
   }
 
   aimMove() {
-
     const all = document.querySelectorAll('.enemy');
     // log(
     //   all[1 ].getAttribute('position')
@@ -138,7 +131,7 @@ export class ArComponent implements OnInit {
       // position.x = 0.5;
       // ownPosition.y = position.y
       // log(ownPosition.y += Math.random()*2-1)
-      ownPosition.y += Math.random() - 0.1
+      ownPosition.y += Math.random() - 0.4
       ownPosition.x += Math.random() - 0.1
       ownPosition.z += Math.random() - 0.1
 
@@ -151,9 +144,34 @@ export class ArComponent implements OnInit {
         Math.abs(ownPosition.z)
       )
       // log(ownPosition, toFar)
-      if (toFar > 200) x.parentNode.removeChild(x);
+      if (toFar > 100) this.toBegin(x) //this.remove(x) //x.parentNode.removeChild(x);
     })
   }
+
+  remove(x: any) {
+    // AFRAME.registerComponent('light', {
+    //   // ...
+    //   remove:  () =>  {
+    //     x.removeObject3D('light');
+    //   }
+    //   // ...
+    // });
+
+    x.parentNode.removeChild(x);
+    x = null; //delete x;
+  }
+
+  toBegin(x) {
+    x.setAttribute('position', this.getStartPosition()); 
+  }
+
+  getPosition(x) {
+
+  }
+  setPosition(x) {
+
+  }
+
 
   getMarker: any = () => document.querySelector('#marker');
 
@@ -162,4 +180,13 @@ export class ArComponent implements OnInit {
     return { x: '0', y: '0', z: '-100' }
 
   }
+
+  getStartPosition() {
+    return { x: '0', y: '0', z: '-100' }
+  }
+
+  launch(){
+
+  }
+
 }
