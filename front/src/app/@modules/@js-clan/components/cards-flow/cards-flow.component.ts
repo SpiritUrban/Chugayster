4fa1370e0cd4735e247af72a739d1c9de3dce34c
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2020-07-06 22:46:32 +0300

    slides moving

--- front/src/app/@modules/@js-clan/components/cards-flow/cards-flow.component.ts
+++ front/src/app/@modules/@js-clan/components/cards-flow/cards-flow.component.ts
@@ -1,6 +1,7 @@
 import { OnInit, Component, EventEmitter, Input, Output } from '@angular/core';
 import appState from '../../../../app-state';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { log } from 'src/app/my_modules/stuff';
 
 
 @Component({
@@ -20,12 +21,22 @@ export class CardsFlowComponent implements OnInit {
     public sanitizer: DomSanitizer
   ) {
     this.st = appState
-   }
+  }
 
   ngOnInit() {
     // this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url + this.card.video);
+  }
 
-
+  moveCards(direction) {
+    log(direction);
+    if(direction == 'left') {
+      const item = this.videos.shift()
+      this.videos.push(item)
+    }
+    if(direction == 'right') {
+      const item = this.videos.pop()
+      this.videos.unshift(item)
+    }
   }
 
 }
