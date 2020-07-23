d6119f50f5365abcdc63c0b96e06c7f6ed5a4a26
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2020-07-23 23:37:26 +0300

    clean up

--- front/src/app/@modules/@training-apparatus/pages/step-a/step-a.component.ts
+++ front/src/app/@modules/@training-apparatus/pages/step-a/step-a.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { log, my_alert } from 'src/app/my_modules/stuff';
-
 declare var window;
 declare var webkitSpeechRecognition;
-
 
 @Component({
   selector: 'app-step-a',
@@ -11,17 +9,15 @@ declare var webkitSpeechRecognition;
   styleUrls: ['./step-a.component.sass']
 })
 export class StepAComponent implements OnInit {
+  
   SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
   talker = new this.SpeechRecognition();
-
   results: [string];
   talking: boolean;
 
   constructor() {
-
     // this.recognition.continuous = true;
     // this.recognition.interimResults = true;
-
   }
 
   ngOnInit(): void {
@@ -33,14 +29,11 @@ export class StepAComponent implements OnInit {
       this.talker.onstart = function () {
         log('onstart')
         this.talking = true;
-        // micButton.style.color = "#B6412C";
       }
 
       this.talker.onend = function () {
         log('onend')
-
         this.talking = false;
-        // micButton.style.color = "black";
       }
 
       this.talker.onresult = function (clip) {
@@ -48,61 +41,30 @@ export class StepAComponent implements OnInit {
         console.log(clip)
       }
 
-      this.talker.onerror = function(event) { 
+      this.talker.onerror = function (event) {
         log(event)
-       }
+      }
 
     }
   }
 
-   startButton(event) {
-     log(1123)
+  startButton(event) {
+    log(1123)
     if (this.talking) {
       log(' make stop')
-    this.talking = false;
-
+      this.talking = false;
       this.talker.stop();
       return;
     }
-    // transcript = '';
     log('start')
     this.talking = true;
     this.talker.start();
   }
 
-
-  // start = () => {
-  //   console.log('start')
-  //   this.recognition.start();
-  //   // button.textContent = "Stop listening";
-  //   // main.classList.add("speaking");
-  // };
-
-  // stop = () => {
-  //   this.recognition.stop();
-  //   // button.textContent = "Start listening";
-  //   // main.classList.remove("speaking");
-  // };
-
   // onResult = event => {
-  //   console.log(event)
-  //   for (const res of event.results) {
-  //     const text = document.createTextNode(res[0].transcript);
-  //     if (res.isFinal) {
-  //       this.results.push()
-  //     }
-  //   }
-
   //   this.recognition.continuous = true;
   //   this.recognition.interimResults = true;
   //   this.recognition.addEventListener("result", this.onResult);
   // };
-
-
-  // startStop() {
-  //   this.listening ? this.stop() : this.start();
-  //   this.listening = !this.listening;
-  // }
-
 
 }
